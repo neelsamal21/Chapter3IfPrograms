1a77c0f948fee38798c751fbc74d60f0f2106aa8
neelsamal21 <nsamal@stu.naperville203.org>
2024-09-26 20:42:08 -0500

    home

--- PlanetProgram.java
+++ PlanetProgram.java
@@ -10,7 +10,7 @@ public class PlanetProgram
     public static void main(String[] args)
     {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("What is your weight on Earth ?: ");
+        System.out.println("What is your weight on Earth (In lbs) ?: ");
         double weight = keyboard.nextDouble();
         
         char choice;
@@ -19,7 +19,6 @@ public class PlanetProgram
         System.out.println("M) Moon ");
         System.out.println("m) Mercury ");
         System.out.println("V) Venus ");
-        System.out.println("E) Earth ");
         System.out.println("R) Mars ");
         System.out.println("J) Jupiter ");
         System.out.println("S) Saturn ");
@@ -27,13 +26,33 @@ public class PlanetProgram
         System.out.println("N) Neptune");
         
         choice = keyboard.next().charAt(0);// first character 
-        
+        DecimalFormat format = new DecimalFormat("#.##");
         switch(choice)
         {
             case 'M':
-                System.out.println("Your weight on the Moon is : "+(weight/6) );
+                System.out.println("Your weight on the Moon is : "+format.format(( weight/6)) + " pounds." );
+                break;
             case 'm':
-                System.out.println("Your weight on Mercury is : " + (weight*.38) );
+                System.out.println("Your weight on Mercury is : " + format.format(( weight*.38) )+ " pounds." );
+                break;
+            case 'V':
+                System.out.println("Your weight on Venus is: " + format.format(( weight*.91 ))+ " pounds." );
+                break;
+            case 'R':
+                System.out.println("Your weight on Mars is: "+format.format(( weight*.38))+ " pounds."  );
+                break;
+            case 'J':
+                System.out.println("Your weight on Jupiter is: "+ format.format((weight*2.34))+ " pounds." );
+                break;
+            case 'S':
+                System.out.println("Your weight on Saturn is :" +format.format((weight*1.06))+ " pounds." );
+                break;
+            case 'U':
+                System.out.println("Your weight on Uranus is :" +format.format((weight*.92))+ " pounds." );
+                break;
+            case 'N':
+                System.out.println("Your weight on neptune is :" +format.format(( weight*1.19))+ " pounds." );
+                break;
                 
             
         }
